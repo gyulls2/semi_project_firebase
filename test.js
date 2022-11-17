98b283e311f618f2d54bc7dd6755a6275a38eb57
“gimgyuri” <kuri6233@gmail.com>
2022-11-17 17:25:25 +0900

    221117 ✍️ 책갈피 생성, 모달 기능 구현

--- test.js
+++ test.js
@@ -1,45 +1,57 @@
 const REST_API_KEY = "8ff342306e1b7d862d5f75fbc30bb978";
 let bookInfo = {};
 
+// 도서정보 get으로 읽어와서 책갈피 div 생성
+  $.get("http://localhost:4000/books", function(data){
+    // console.log(JSON.stringify(data), status);
+    data.forEach((response) => {
+      createDIV(response)
+    });
+  });
+
 
 // 검색 버튼 클릭 시 GET으로 도서 api 요청
-document.getElementById("search").addEventListener("click", function(){
+function search(){
+  let bookName = document.getElementById("bookName").value;
+    $.ajax({
+      method: "GET",
+      url: "https://dapi.kakao.com/v3/search/book?target=title",
+      data: { query : bookName },
+      headers: {Authorization: `KakaoAK ${REST_API_KEY}`}
+    })
+      .done(function( book) {
+
+        let title = book.documents[0].title;
+        let thumbnail = book.documents[0].thumbnail;
+        let authors = book.documents[0].authors;
+        let contents = book.documents[0].contents;
+
+
+        // bookInfo 객체에 검색 도서 정보 저장
+        bookInfo.title = title;
+        bookInfo.thumbnail = thumbnail;
+        bookInfo.authors = authors;
+        bookInfo.contents = contents;
+        console.log(bookInfo); 
   
-let bookName = document.getElementById("bookName").value;
-  $.ajax({
-    method: "GET",
-    url: "https://dapi.kakao.com/v3/search/book?target=title",
-    data: { query : bookName },
-    headers: {Authorization: `KakaoAK ${REST_API_KEY}`}
-  })
-    .done(function( book ) {
-      let title = book.documents[0].title;
-      let thumbnail = book.documents[0].thumbnail;
-      let authors = book.documents[0].authors;
-      let contents = book.documents[0].contents;
-
-      // bookInfo 객체에 검색 도서 정보 저장
-      bookInfo.title = title;
-      bookInfo.thumbnail = thumbnail;
-      bookInfo.authors = authors;
-      bookInfo.contents = contents;
-      console.log(bookInfo); 
-
-
-      // 도서 썸네일 이미지, 제목 출력
-      document.getElementById("title").innerHTML = book.documents[0].title;
-      document.getElementById("thumbnail").src = book.documents[0].thumbnail;
-
-      // 코멘트 입력폼 노출
-      document.getElementById("commentForm").style.display = "block";
-    });
-});
+  
+        // 도서 썸네일 이미지, 제목 출력
+        document.getElementById("title").innerHTML = book.documents[0].title;
+        document.getElementById("thumbnail").src = book.documents[0].thumbnail;
+  
+        // 코멘트 입력폼 노출
+        document.getElementById("commentForm").style.display = "block";
+      })
+      .fail(function(e){
+        console.log(e.responseJSON);
+        alert("찾으시는 책 정보가 없습니다.");
+      });
+}
 
 
 // 코멘트 저장 버튼 클릭 시 이벤트
-let form = document.getElementById ("commentForm");
-form.addEventListener("submit", function(e){
-  e.preventDefault();
+function saveComment(){
+  // e.preventDefault();
 
   let comment = document.getElementById("comment").value;
   bookInfo.comment = comment;
@@ -58,45 +70,45 @@ form.addEventListener("submit", function(e){
   })
   .done(function(response) {
     console.log(response);
+    createDIV(response)
+    });
+}
 
-    // container 안에 자식 노드로 div 생성
-    let bookMark = document.createElement("div");
-    bookMark.setAttribute("class", "bookMark");
-    document.getElementById("container").appendChild(bookMark);
-    bookMark.innerHTML = `<div class='commentBox'>${response.comment}</div>
-    <div class='titleBox'>${response.title}</div>`;
+
+
+
+  // container 안에 자식 노드로 div 생성
+function createDIV(response){
+  let bookMark = document.createElement("div"); 
+  bookMark.setAttribute("class", "bookMark");
+  bookMark.setAttribute("onclick", "showModal(this)");
+  document.getElementById("container").appendChild(bookMark);
+  bookMark.innerHTML = `<div class='commentBox'>${response.comment}</div>
+  <div class='titleBox'>${response.title}</div>`;
+}
+
+
+// 모달 띄우기
+function showModal(div){
+  div.setAttribute("data-bs-toggle", "modal");
+  div.setAttribute("data-bs-target", "#bookInfoModal");
+  
+  modalInput(div)
+}
+
+// 모달에 정보 전송
+function modalInput(div){
+  // 현재 제목
+  thisTitle = div.childNodes[2].outerText;
+
+  $.get("http://localhost:4000/books", function(data){
+    data.forEach((response) => {
+      if(thisTitle == response["title"]){
+        document.getElementById("modalThumbnail").src = response["thumbnail"];
+        document.getElementById("modalTitle").innerHTML = `<b>제목</b> : ${response["title"]}`;
+        document.getElementById("modalAuthors").innerHTML = `<b>작가</b> : ${response["authors[]"]}`;
+        document.getElementById("modalContents").innerHTML = `<b>줄거리</b> : ${response["contents"]} ...`;
+      }
     });
   });
-
-
-
-
-
-// 도서정보 get으로 읽기
-// $.ajax({
-//   url: "http://localhost:4000/books", //주소
-//   data: bookInfo, //전송 데이터
-//   type: "GET", //전송 타입    		
-// })
-
-
-
-// $.get("demo_test.asp", function(data, status){
-//   alert("Data: " + data + "\nStatus: " + status);
-// });
-
-// fetch("http://localhost:3000/books")
-// 	.then((response) =>response.json())
-// 	.then((data) => console.log(data))
-// 	.catch((error) => console.log(error));
-
-
-
-
-
-
-
-
-
-
-
+}
